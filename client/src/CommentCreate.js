1d5ff59366b3c7d4bfcf9a65c67029c4b395eb7c
varunraj@CORP.AMAZONWORKSPACES.COM <varunraj@CORP.AMAZONWORKSPACES.COM>
2020-09-15 14:36:09 -0400

    completed client

---
+++ client/src/CommentCreate.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+import axios from 'axios'
+
+// comment create should get the postId to associate the comment o
+
+export default ({postId}) => {
+
+    const [content, setContent] = useState('');
+
+
+    const onSubmit = async (event) => {
+        
+        event.preventDefault();  
+        await axios.post(`http://localhost:4001/posts/${postId}/comments`,{
+            content
+        });
+
+        setContent('');
+    };
+
+
+
+
+    return(
+        <div>
+            <form onSubmit={onSubmit}>
+                <div className="form-group">
+                    <label>New Comment</label>
+                    <input 
+                        className="form-control"
+                        onChange={e=> setContent(e.target.value)}
+                    
+                    />
+                </div>
+                <button className="btn btn-primary">Submit</button>    
+            </form>
+        </div>
+    )
+};
+
+
