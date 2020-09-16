dcc15ec38d57cec36ed09dcf2796a681e5dcb084
varunraj <varunraj@CORP.AMAZONWORKSPACES.COM>
2020-09-16 16:30:04 -0400

    added event-bus and query services

--- posts/index.js
+++ posts/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const {randomBytes} = require('crypto');
 const cors = require('cors');
 const app = express();
+const axios = require('axios');
 
 
 app.use(bodyParser.json());
@@ -14,7 +15,7 @@ app.get('/posts',(req,res)=>{
     res.send(posts)
 })
 
-app.post('/posts', (req,res)=>{
+app.post('/posts', async (req,res)=>{
 
     const id = randomBytes(4).toString('hex');
     const {title} = req.body;
@@ -23,10 +24,22 @@ app.post('/posts', (req,res)=>{
                     id,
                     title 
                 }
+            
+    await axios.post('http://localhost:4005/events',{
+        type:'PostCreated',
+        data: {
+            id, title
+        }
+    })
     
     res.status(201).send(posts[id]);
 
 });
+
+app.post('/events',(req,res)=>{
+    console.log("Received Event", req.body.type);
+    res.send({});
+})
 
 app.listen(4000, ()=>{
     console.log("Listening to 4000")
